161846bea88c9edd57337cf32a61468508b5f524
ChanLee <dlcks113@gmail.com>
2022-08-09 02:34:01 +0900

    gitigonre 추가, 필요없는 파일 삭제

--- test/대신증권/main.py
+++
@@ -1,8 +0,0 @@
-import 예수금_주문가능금액 as deposit_orderable
-import 보유종목정보
-
-result = 보유종목정보.myStock_Info('A005930')
-#deposit_orderable.deposit()
-#deposit_orderable.orderable_account()
-
-print(result)
--- test/대신증권/temp.py
+++
@@ -1,5 +0,0 @@
-import num_to_Korean as n2K
-
-n2K.digit2txt("11123432.123")
-
-print((14.00).is_integer())
